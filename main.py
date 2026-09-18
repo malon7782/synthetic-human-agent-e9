8dd83ae3ebf9b92790ffba12834c1e4fa14f7f32
Tian Yuchen <cat@malon.dev>
2026-09-18 15:36:13 +0800

    main: add random task selection mechanism
    although there is only one task available (and not even fully implemented),
    we still need a mechanism to randomly select tasks to perform.
    
    every task should be regitered in task_list (which lives in main.py),
    and once main.py is evoked, an infinite loop starts to roll tasks and call
    corresponding Class initializer from the registry... quite simple, isn't it?

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import random
 from core.timing import Timing
 from core.process import Process
 from core.ui import UI
 from tasks.browser import BrowserTask
 
+
+
 def main():
     timing = Timing()
     process = Process()
     ui = UI(timing)
-    
-    task = BrowserTask(process, ui, timing)
-    task.run()
-    print("done.")
+
+    # registry of tasks
+    task_list = {
+        'browser': BrowserTask(process, ui, timing)
+    }
+
+    while True:
+        # randomly select a task
+        task_list[random.choice(list(task_list.keys()))].run()
+        timing.pause()
+        print("done.")
 
 if __name__ == "__main__":
     main()
