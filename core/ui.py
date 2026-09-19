3865dd51d220a67c4093524f9f4736114c2f1556
Tian Yuchen <cat@malon.dev>
2026-09-19 16:21:35 +0800

    ui: add serveral useful APIs:
     - is_on_desktop(): unless all existing windows are minimized, return
    false.
    
     - press_hotkey(): the function receives unlimited number of strings
    , then try to find and press them as key combinations on the keyboard.
    note that to implement this function, key mappings are introduced on
    top of ui.py.
    
    this commit prepares for the introduction of the 'default' task.

--- core/ui.py
+++ core/ui.py
@@ -1,3 +1,13 @@
+import ctypes
+
+VK_MAP = {'win': 0x5B, 'ctrl': 0x11, 'alt': 0x12, 'shift': 0x10,
+          'esc': 0x1B, 'enter': 0x0D, 'tab': 0x09}
+
+# {'a': 65, 'b': 66, 'c': 67....}
+VK_MAP.update({c: ord(c.upper()) for c in "abcdefghijklmnopqrstuvwxyz"})
+KEYEVENTF_KEYUP = 0x0002
+
+
 class UI:
     def __init__(self, timing):
         self.timing = timing
@@ -28,3 +38,20 @@ class UI:
             # thinking now and then... perhaps we need to simulate
             # that, too.
             print(f"typing: {char}")
+
+    def is_on_desktop(self):
+        hwnd = ctypes.windll.user32.GetForegroundWindow()
+        name = ctypes.create_unicode_buffer(64)
+        ctypes.windll.user32.GetClassNameW(hwnd, name, 64)
+        # name of the Desktop should be either Program or WorkerW
+        return name.value in ("Progman", "WorkerW")
+
+    def press_hotkey(self, *keys):
+        # e.g. press_hotkey('win', 'd') to show desktop
+        vks = [VK_MAP[k] for k in keys]
+        for vk in vks:
+            ctypes.windll.user32.keybd_event(vk, 0, 0, 0)
+        for vk in reversed(vks):
+            # release
+            ctypes.windll.user32.keybd_event(vk, 0, KEYEVENTF_KEYUP, 0)
+        self.timing.delay()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from core.timing import Timing
 from core.process import Process
 from core.ui import UI
 from tasks.browser import BrowserTask
+from tasks.default import DefaultTask
 
 
 
@@ -11,12 +12,16 @@ def main():
     process = Process()
     ui = UI(timing)
 
+    # the special one
+    default_task = DefaultTask(ui, timing)
     # registry of tasks
     task_list = {
         'browser': BrowserTask(process, ui, timing)
     }
 
     while True:
+        # set everything to the default status
+        default_task.run()
         # randomly select a task
         task_list[random.choice(list(task_list.keys()))].run()
         timing.pause()
